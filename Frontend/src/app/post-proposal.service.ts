80ade73915e857d9e8f0ddca892801622f0ce267
ks8346 <kartik.sachdeva@soprabanking.com>
2020-11-06 15:43:13 +0530

    error handling in api's

--- Frontend/src/app/post-proposal.service.ts
+++ Frontend/src/app/post-proposal.service.ts
@@ -6,14 +6,19 @@ import {Proposal} from './proposal'
 })
 export class PostProposalService {
   public sentTeams:{}[]
-  private url="http://localhost:8080/proposal/add"
+  private createUrl="http://localhost:8080/proposal/add";
+  private updateUrl="";
   constructor(private http:HttpClient) { }
-  postProposal(data,userId,teams){
+  postProposal(data,userId){
+    data.userId=userId
     if(!data.key){
       data.key="null"
+      console.log(data)
+      return this.http.post(this.createUrl,data)
     }
-    data.userId=userId
-    console.log(data)
-    return this.http.post(this.url,data)
+    else{
+      return this.http.post(this.updateUrl,data)
+    }
+    
   }
 }
