24055eee718f6c6fcd76e134f1441e899749e54c
Priyank094 <38767826+Priyank094@users.noreply.github.com>
2020-11-06 16:43:09 +0530

    Comment Component

--- Frontend/src/app/landing-page/feed/comment/comment.component.ts
+++ Frontend/src/app/landing-page/feed/comment/comment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input,Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-comment',
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment.component.css']
 })
 export class CommentComponent implements OnInit {
+  @Input() comment:Comment;
+  public commentId;
+  public userName;
+  public commentData;
+  public commentedUserId:number;
+  @Input() userId:number;
+
+  @Output() 
+ parentFunction:EventEmitter<any> = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit(): void {
+    // console.log(this.comment)
+    this.commentData = this.comment.comment;
+    this.commentId = this.comment.id;
+    this.userName = this.comment.user.name;
+    this.commentedUserId = this.comment.user.id;
+
+    
+  }
+
+  deleteComment()
+  {
+      //  console.log(this.commentId)
+
+       this.parentFunction.emit(this.commentId) 
   }
 
 }
