2f682beb4f26bda901dc1cefc28234060c79fc0c
ks8346 <kartik.sachdeva@soprabanking.com>
2020-11-08 23:24:19 +0530

    Testing in progress

--- Frontend/src/app/post.spec.ts
+++ Frontend/src/app/post.spec.ts
@@ -2,6 +2,6 @@ import { Post } from './post';
 
 describe('Post', () => {
   it('should create an instance', () => {
-    // expect(new Post(new Date(),"This is desc",1,[],"This is title",10,{})).toBeTruthy();
+    expect(new Post("This is desc",1,[],"This is title",10,{id:1,name:"kartik",email:"kartik@gmail.com",teams:{id:1,name:"devs"}})).toBeTruthy();
   });
 });
--- Frontend/src/app/teams.service.spec.ts
+++ Frontend/src/app/teams.service.spec.ts
@@ -1,17 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TeamsService } from './teams.service';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 describe('TeamsService', () => {
   let service: TeamsService;
-
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule]
+    });
     service = TestBed.inject(TeamsService);
+    httpClient=TestBed.inject(HttpClient)
+    httpTestingController=TestBed.inject(HttpTestingController)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  it("should get all the teams",()=>{
+    const teams=[{id:1,name:"Devs"},{id:2,name:"Devs-Frontend"}]
+    service.getTeams().subscribe((data)=>expect(data).toEqual(teams))
+  })
   
 });
