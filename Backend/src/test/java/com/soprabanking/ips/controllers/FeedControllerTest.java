16577378983b183570df128a64664691862c4dee
Prashant Kumar Singh <pks070198@gmail.com>
2020-11-08 19:37:01 +0530

    feed service tests

--- Backend/src/test/java/com/soprabanking/ips/controllers/FeedControllerTest.java
+++ Backend/src/test/java/com/soprabanking/ips/controllers/FeedControllerTest.java
@@ -19,6 +19,7 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -29,6 +30,7 @@ import com.soprabanking.ips.models.User;
 import com.soprabanking.ips.repositories.TeamRepository;
 import com.soprabanking.ips.repositories.UserRepository;
 import com.soprabanking.ips.services.FeedService;
+import com.soprabanking.ips.services.FeedServiceTest;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -39,84 +41,36 @@ public class FeedControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+    
+    private FeedServiceTest feedServiceTest = new FeedServiceTest();
 
     @Mock
     private FeedService feedService;
 
-    @Mock
-    private UserRepository userRepository;
-
-    @Mock
-    private TeamRepository teamRepository;
-
     @InjectMocks
     private FeedController feedController;
 
-    @Test
-    public void testSave() throws Exception {
-		
-		/*Date date = new Date();
-		Team team = new Team();
-		team.setName("1");
-		
-		User user = new User();
-		user.setEmail("sa");
-		user.setTeam(team);
-		
-		when(teamRepository.save(team)).thenReturn(team);
-		when(userRepository.save(user)).thenReturn(user);
-		
-		//user.setId(1);;
-		team.setId((long) 1);
-		user.setTeam(team);
-		when(userRepository.getOne((long) 1)).thenReturn(user);
-		
-		MvcResult result =  mockMvc.perform(get("/feed/save")
-				.content(objectMapper.writeValueAsString(user)))
-			.andExpect(status().isOk())
-			.andReturn();*/
-
-        //assertThat(result.getResponse().getContentAsString())
-        //.isEqualToIgnoringWhitespace(objectMapper.writeValueAsString(user));
-    }
-
-    /////Error with parameters
+   
     @Test
     public void testGetAllProposalFeed() throws Exception {
-
-
-        String dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
-        //Date date = new SimpleDateFormat(dateFromat).parse("2020-10-29T16:08:59.962+05:30");
-
-        Date now = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
-        sdf.setTimeZone(TimeZone.getTimeZone("IST"));
-
-        String ds = sdf.format(now);
 
         Proposal proposal1 = new Proposal();
         Proposal proposal2 = new Proposal();
 
-        System.out.println(now);
-        System.out.println(ds);
-
         List<Proposal> proposals = new ArrayList<>();
         proposals.add(proposal1);
         proposals.add(proposal2);
-		
-		/*when(feedService.fetchAllProposals(now, now, 0, 5))
-		.thenReturn(proposals);*/
+        String body = feedServiceTest.createAllFeedParams(new Date().toString(), "all");
+        
+        when(feedService.fetchAllProposals(body)).thenReturn(proposals);
 
         MvcResult actualResult = mockMvc.perform(post("/feed/all")
-                .param("startDate", ds)
-                .param("endDate", ds)
-                .param("page", "0")
-                .param("size", "5"))
-                .andExpect(status().is4xxClientError())
+        		.contentType(MediaType.APPLICATION_JSON)
+                .content(body))
+                .andExpect(status().isOk())
                 .andReturn();
 
-        System.out.println(actualResult.getResponse().getContentAsString());
-        //assertThat(actualResult.getResponse().getContentAsString())
-        //.isEqualToIgnoringWhitespace(objectMapper.writeValueAsString(proposals));
+        assertThat(actualResult.getResponse().getContentAsString())
+        .isEqualToIgnoringWhitespace(objectMapper.writeValueAsString(proposals));
     }
 }
