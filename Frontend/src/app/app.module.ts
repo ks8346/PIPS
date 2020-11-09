a6ffa6a3df0608eea7beaaa612fe84c889720df1
Ankit rustagi <ankitrustagi12@gmail.com>
2020-11-09 14:46:54 +0530

    Registration Component Removed

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { HomeComponent } from './home/home.component';
@@ -48,7 +47,6 @@ import { MatSelectFilterModule } from 'mat-select-filter';
   declarations: [
     AppComponent,
     LoginComponent,
-    RegistrationComponent,
     HomeComponent,
     ErrorComponent,
     RegisterComponent,
