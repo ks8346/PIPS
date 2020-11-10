57a43b07c4d5a96d5db4d6b6373fe2cf2595526e
ksh-t-z <kshitij.goel04@gmail.com>
2020-11-10 14:29:40 +0530

    Revert "Merge branch 'main' of https://github.com/ankitrustagi12/Process-Improvement-Proposal-System into main"
    This reverts commit 9ec9bf57b327198e28f2d25b2c78adff19791c19, reversing
    changes made to 26f08917252378160fc8452a292a9e32259625d7.

--- Backend/src/main/java/com/soprabanking/ips/config/MyConfig.java
+++ Backend/src/main/java/com/soprabanking/ips/config/MyConfig.java
@@ -1,16 +1,6 @@
 package com.soprabanking.ips.config;
 
- 
-
 import java.util.concurrent.locks.ReentrantLock;
-
-import javax.transaction.Transactional;
-
- 
-
-//import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-
- 
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,85 +13,59 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
- 
-
 @Configuration
 @EnableWebSecurity
-@Transactional
-//@EnableOAuth2Sso
 public class MyConfig extends WebSecurityConfigurerAdapter {
-
- 
 
     @Bean
     public UserDetailsService getUserDetailService() {
         return new UserDetailsServiceImpl();
     }
-    @Bean     
-    public ReentrantLock getLock() {         
-    	return new ReentrantLock();     
-    	}
- 
+    
+    @Bean
+    public ReentrantLock getLock() {
+        return new ReentrantLock();
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
- 
-
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
 
- 
-
         daoAuthenticationProvider.setUserDetailsService(this.getUserDetailService());
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
 
- 
-
         return daoAuthenticationProvider;
-
- 
 
     }
 
- 
-
     /// configure method...
-
- 
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider());
 
- 
-
     }
 
- 
 
-    
-    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
-        .antMatchers("/user/**").hasRole("USER")
-        //.antMatchers("/signin").hasRole("USER")
-        .antMatchers("/**").permitAll().anyRequest()
-               
+                .antMatchers("/user/**").hasRole("USER")
+                //.antMatchers("/signin").hasRole("USER")
+                .antMatchers("/**").permitAll().anyRequest()
+
                 .authenticated().and()
-                .formLogin().and()
-                .httpBasic()
+                .formLogin()
                 //.defaultSuccessUrl("/user/index")
                 //.loginPage("/signin").defaultSuccessUrl("/user/index")
-                .and().oauth2Login()
-                ;
+                .and()
+                .httpBasic();
     }
-
- 
 
 
 }
