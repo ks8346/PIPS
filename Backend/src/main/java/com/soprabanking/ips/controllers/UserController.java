7f186b0ae0bbbd0203aa504c4ad3a4ae7fd31477
Prashant Kumar Singh <pks070198@gmail.com>
2020-11-11 12:05:18 +0530

    merge

--- Backend/src/main/java/com/soprabanking/ips/controllers/UserController.java
+++ Backend/src/main/java/com/soprabanking/ips/controllers/UserController.java
@@ -1,50 +1,29 @@
 package com.soprabanking.ips.controllers;
 
- 
-
- 
-
 import java.security.Principal;
 import java.util.concurrent.ThreadLocalRandom;
-
- 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
- 
-
+import com.soprabanking.ips.helper.UserAuth;
+import com.soprabanking.ips.models.User;
 import com.soprabanking.ips.repositories.TeamRepository;
 import com.soprabanking.ips.repositories.UserRepository;
-import com.soprabanking.ips.helper.ResponseMessage;
-import com.soprabanking.ips.helper.UserAuth;
-import com.soprabanking.ips.models.Team;
-import com.soprabanking.ips.models.User;
-
- 
-
- 
 
 
 @CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping("/user")
-
- 
-
 public class UserController
 {
 
