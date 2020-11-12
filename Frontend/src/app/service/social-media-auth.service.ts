a37654330fb1330cbd25f95d2bdd8942366d40fd
ksh-t-z <kshitij.goel04@gmail.com>
2020-11-12 18:55:57 +0530

    Final Changes

--- Frontend/src/app/service/social-media-auth.service.ts
+++ Frontend/src/app/service/social-media-auth.service.ts
@@ -1,3 +1,5 @@
+import { TransferDataService } from './transfer-data.service';
+import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
@@ -10,26 +12,39 @@ export class SocialMediaAuthService {
   public username: String;
 
   public res;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private router: Router,
+    private transferDataService:TransferDataService) { }
 
-  socialMedia(data){
-    console.log("inside socialMedia(Frontend)")
-    return this.http.post("http://localhost:8080/user/fbTeam",data["data1"],{responseType:'text' as 'json'})
-  }
-
-  // socialMedia(data) {
-  //   return this.http.post(`http://localhost:8080/fbTeam`,data).pipe(map((res) => {
-  //       this.res=res;
-  //       this.username = res["username"];
-  //       this.registerSuccessfulLogin(this.username);
-  //     }));
+  // socialMedia(data){
+  //   console.log("inside socialMedia(Frontend)")
+  //   return this.http.post("http://localhost:8080/getSocialInfo",data["data1"],{responseType:'text' as 'json'})
   // }
+
+  socialMedia(data) {
+    console.log("inside Social Media")
+    return this.http.post(`http://localhost:8080/getSocialInfo`,data).pipe(map((res) => {
+        this.res=res;
+        console.log(res)
+        // this.username = res["name"];
+        this.registerSuccessfulLogin();
+      }
+      // ,
+      // (error)=>{
+      //   console.log("error------",error['error']);
+      //   //team page
+      //   this.transferDataService.emitData(error['error'])
+      //   this.router.navigate(['/team']);
+  
+      // }
+      ));
+  }
 
   createBasicAuthToken(username: String) {
     return 'Basic ' + (username)
   }
 
-  registerSuccessfulLogin(username) {
+  registerSuccessfulLogin() {
     sessionStorage.setItem(this.USER_NAME_SESSION_ATTRIBUTE_NAME, this.res.message)
   }
 
---
+++ Frontend/src/app/service/transfer-data.service.ts
@@ -0,0 +1,17 @@
+
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class TransferDataService {
+userData: any;
+  //observer = new Subject();
+//  public subscriber = this.observer.asObservable();
+
+  emitData(data) {
+    console.log("inside service-------",data);
+    this.userData = data;
+
+   // this.observer.next(data);
+  }
+}
