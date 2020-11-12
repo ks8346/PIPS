cdf812808be73cc4e5845c4040f2bd7f55fe8a8e
Prashant Kumar Singh <pks070198@gmail.com>
2020-11-12 15:16:11 +0530

    merge

--- Backend/src/main/java/com/soprabanking/ips/controllers/FeedController.java
+++ Backend/src/main/java/com/soprabanking/ips/controllers/FeedController.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +26,8 @@ import com.soprabanking.ips.services.ProposalService;
 @CrossOrigin
 @RequestMapping("/feed")
 public class FeedController {
+	
+	private static Logger LOGGER = LogManager.getLogger(FeedController.class);
 
     @Autowired
     private FeedService feedService;
@@ -38,13 +42,17 @@ public class FeedController {
     public ResponseEntity<List<Proposal>> getAllProposalFeed(@RequestBody String body) {
 
         try {
+        	LOGGER.info("Inside FeedController : getAllProposalFeed() method");
         	List<Proposal> list = feedService.fetchAllProposals(body);
         	if(list.isEmpty())
         		throw new Exception();
+        	
+        	LOGGER.info("Inside FeedController : getAllProposalFeed() SUCCESSFULL");
             return new ResponseEntity<List<Proposal>>(list,
                     HttpStatus.OK);
         	
         } catch (Exception e) {
+        	LOGGER.info("Inside FeedController : getAllProposalFeed() FAILED");
             return new ResponseEntity<List<Proposal>>(new ArrayList<>(), HttpStatus.NOT_ACCEPTABLE);
 
         }
