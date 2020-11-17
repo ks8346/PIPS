01e72974e6a03ec72a0f8b4273a0db5ccce01102
Priyank094 <38767826+Priyank094@users.noreply.github.com>
2020-11-17 16:39:38 +0530

    documentation

--- Frontend/src/app/proposal.ts
+++ Frontend/src/app/proposal.ts
@@ -1,4 +1,13 @@
+/** This is a proposal class resposible for handling proposal data */
 export class Proposal {
+    /**
+     * this is the constructor of proposal class
+     * @param userId This is the UserId from session.
+     * @param title This is the title of the Proposal.
+     * @param description This is the description of the Proposal.
+     * @param teams This is the array of teams to which it shared.
+     * @param {number} id This is the Proposal id. 
+     */
     constructor(
         public userId:string,
         public title:string,
